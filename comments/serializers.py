ad8685a49c0bbf786797ec04c2537b9f33c0fbe1
lestece <loiceleste94@gmail.com>
2023-06-06 12:47:29 +0000

    Creat the comments app, write the model and serializers

---
+++ comments/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.id')
+    profile_image = serializers.ReadOnlyField(source='owner.image')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'profile_id', 'profile_image', 'created_at', 
+            'updated_at', 'post', 'content', 'is_owner',
+        ]
+
+    
+class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')
